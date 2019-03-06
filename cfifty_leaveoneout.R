77c7415d2c645e8b6898ed05ff5ad9b5eea40014
jessegzhang <jessegzhang@gmail.com>
2019-03-06 08:19:23 -0900

    removing parallelization of cfifty_leaveoneout

--- cfifty_leaveoneout.R
+++ cfifty_leaveoneout.R
@@ -22,7 +22,7 @@ cfifty_training <- function(removal_point,data_set, predict_pointer, col_name ){
 }
 
 leaveoneoutCI<- function(data_set, predict_pointer ){
-  cl <- makeCluster(16, type = "SOCK", outfile="debug_leaveoneout_cfifty.txt") 
+  #cl <- makeCluster(16, type = "SOCK", outfile="debug_leaveoneout_cfifty.txt") 
   clusterEvalQ(cl, {library(caret); library(C50); library(e1071)})  
   
   data_set[,predict_pointer]<-as.factor(data_set[,predict_pointer])
@@ -30,18 +30,18 @@ leaveoneoutCI<- function(data_set, predict_pointer ){
   cfifty_leaveoneout<-vector(mode = "list", length = nrow(data_set))
   col_name <- colnames(data_set)[predict_pointer]
   
-  oldw <- getOption("warn")
-  options(warn = -1)
-  print("calling cfifty")
-  cfifty_leaveoneout<-parSapply(cl, 1:nrow(data_set), cfifty_training, data_set=data_set, predict_pointer=predict_pointer, col_name=col_name)
-  mean_cfifty<-sum(unlist(cfifty_leaveoneout))/nrow(data_set)
-  sd_cfifty<-sqrt((mean_cfifty*(1-mean_cfifty))/nrow(data_set))
-  options(warn = oldw)
-  
-  # #leave one out cross validation
-  # for(i in 1:nrow(data_set)) {
-  #   train_set<-data_set[-i,]
-  #   test_set<-data_set[i,]
+  #oldw <- getOption("warn")
+  #options(warn = -1)
+  #print("calling cfifty")
+  #cfifty_leaveoneout<-parSapply(cl, 1:nrow(data_set), cfifty_training, data_set=data_set, predict_pointer=predict_pointer, col_name=col_name)
+
+  #options(warn = oldw)
+  formula <- as.formula(paste(col_name, ' ~ .' ))
+  tr_control<- trainControl(method="none", allowParallel = TRUE) 
+  #leave one out cross validation
+  for(i in 1:nrow(data_set)) {
+  train_set<-data_set[-i,]
+  test_set<-data_set[i,]
   #   
   #   knn_fit<-train(formula, data=train_set, method="knn", trControl=tr_control,
   #                  preProcess=c("center","scale"))
@@ -52,12 +52,13 @@ leaveoneoutCI<- function(data_set, predict_pointer ){
   #   svm_guess[i]<-(predict(svm_fit,test_set)==test_set[,predict_pointer])
   #   
   #   
-  #   cfifty_fit<-train(formula, data=train_set, method="C5.0",
-  #                     preProcess=c("center","scale"))
-  #   cfifty_guess[i]<-(predict(cfifty_fit,test_set)==test_set[,predict_pointer])
+  cfifty_fit<-train(formula, data=train_set, method="C5.0",
+                       preProcess=c("center","scale"))
+  cfifty_leaveoneout[i]<-(predict(cfifty_fit,test_set)==test_set[,predict_pointer])
   #   
-  # } 
-  
+  } 
+  mean_cfifty<-sum(unlist(cfifty_leaveoneout))/nrow(data_set)
+  sd_cfifty<-sqrt((mean_cfifty*(1-mean_cfifty))/nrow(data_set))
   
   
   
