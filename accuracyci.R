9a4d59696aa726e521bc1e2c6b59d35885062f48
Zhang <jgz@pn100987.campus.nist.gov>
2019-08-26 17:07:53 -0400

    adding csvs to write scripts for confidence intervals

--- accuracyci.R
+++ accuracyci.R
@@ -5,6 +5,7 @@ require(randomForest)
 require(snow)
 require(e1071)
 require(gower)
+require(kernlab)
 
 
 #take in data
@@ -13,38 +14,42 @@ data_file<-args[1]
 predict_pointer<-as.numeric(args[2])
 data_set<-read.csv(args[1])
 
-
+dir.create(args[3])
 
 #split the data
-data_set[,predict_pointer]<-as.factor(data_set[,predict_pointer])
-data_splits <- createDataPartition(y = data_set[,predict_pointer], p = 0.70,list = FALSE)
-training_set <- data_set[data_splits,]
-testing_set <- data_set[-data_splits,]
+for(i in 1:100){
+  data_set[,predict_pointer]<-as.factor(data_set[,predict_pointer])
+  data_splits <- createDataPartition(y = data_set[,predict_pointer], p = 0.70,list = FALSE)
+  training_set <- data_set[data_splits,]
+  testing_set <- data_set[-data_splits,]
+  my_fpath <- file.path(".",args[3], paste0("ci_train_",toString(i)))
+  dir.create(my_fpath)
+  
+  write.csv(training_set, file.path(my_fpath, args[4]), row.names=FALSE)
+  
+  #train yourself and report population accuracy
+  
+  col_name <- colnames(data_set)[predict_pointer]
+  formula <- as.formula(paste(col_name, ' ~ .' ))
+  tr_control<- trainControl(method="none",allowParallel=TRUE)
+  rf_fit<-train(formula, data=training_set, method="rf", trControl=tr_control,
+                    preProcess=c("center","scale"))
+  svm_fit<-train(formula, data=training_set, method="svmLinear", trControl=tr_control,
+                 preProcess=c("center","scale"))
+  adabag_fit<-train(formula, data=training_set, method="AdaBag", trControl=tr_control,
+                    preProcess=c("center","scale"))
+  
+  rf_predict<-predict(rf_fit,testing_set)
+  svm_predict<-predict(svm_fit,testing_set)
+  adabag_predict<-predict(adabag_fit,testing_set)
+  
+  #write population accuracy into a file
+  
+  svm_mean<-mean(svm_predict==testing_set[,predict_pointer])
+  adabag_mean<-mean(adabag_predict==testing_set[,predict_pointer])
+  rf_mean<-mean(rf_predict==testing_set[,predict_pointer])
+  
+  final_data<-c("svm_accuracy"=svm_mean, "adabag_accuracy"=adabag_mean, "rf_accuracy"=rf_mean)
+  write.csv(final_data, file.path(my_fpath,"true_accuracy.csv"))
 
-write.csv(training_set, file=args[4], row.names=FALSE)
-
-#train yourself and report population accuracy
-
-col_name <- colnames(data_set)[predict_pointer]
-formula <- as.formula(paste(col_name, ' ~ .' ))
-tr_control<- trainControl(method="none",allowParallel=TRUE)
-rf_fit<-train(formula, data=training_set, method="rf", trControl=tr_control,
-                  preProcess=c("center","scale"))
-svm_fit<-train(formula, data=training_set, method="svmLinear", trControl=tr_control,
-               preProcess=c("center","scale"))
-adabag_fit<-train(formula, data=training_set, method="AdaBag", trControl=tr_control,
-                  preProcess=c("center","scale"))
-
-rf_predict<-predict(rf_fit,testing_set)
-svm_predict<-predict(svm_fit,testing_set)
-adabag_predict<-predict(adabag_fit,testing_set)
-
-#write population accuracy into a file
-
-svm_mean<-mean(svm_predict==testing_set[,predict_pointer])
-adabag_mean<-mean(adabag_predict==testing_set[,predict_pointer])
-rf_mean<-mean(rf_predict==testing_set[,predict_pointer])
-
-final_data<-c("svm_accuracy"=svm_mean, "adabag_accuracy"=adabag_mean, "rf_accuracy"=rf_mean)
-dir.create(args[3])
-write.csv(final_data, file.path(".", args[3],"true_accuracy.csv"))
+}
